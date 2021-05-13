b6ca42eaf2887b1af09c22d3cfdaf3f82f4724f3
jammmm91 <ganjinam00@naver.com>
2021-05-13 21:02:15 +0900

    new

---
+++ Spring0513/src/main/java/www/kopo/jaemin/Student.java
@@ -0,0 +1,16 @@
+package www.kopo.jaemin;
+
+public class Student {
+	int idx;
+	String name;
+	int score;
+	
+	Student() {
+		
+	}
+	
+	Student(String name, int score) {
+		this.name = name;
+		this.score = score;				
+	}
+}
