632405201c6c1a40ba014fa6f57b270b3fab0f7a
jaemin <ganjinam00@naver.com>
2021-05-17 01:01:06 +0900

    new

--- BaekJoon/src/BJ2588.java
+++ BaekJoon/src/BJ2588.java
@@ -1,7 +1,4 @@
-import java.awt.List;
-import java.lang.reflect.Array;
-import java.util.Arrays;
-import java.util.Collections;
+
 import java.util.Scanner;
 
 public class BJ2588 {
@@ -10,11 +7,13 @@ public class BJ2588 {
 		int userInput1 = s.nextInt();
 		int userInput2 = s.nextInt();
 		
-		String num = String.valueOf(userInput2);
+		String str = Integer.toString(userInput2);
+		String [] splitStr = str.split("");
 		
-		String [] splitNum = num.split("(?<=.)");
-		for (int i = 0; i < splitNum.length; i++) {
-			System.out.println(userInput1*Integer.parseInt(splitNum[i]));
+				
+		for (int i = splitStr.length-1; i >= 0; i--) {
+			System.out.println(userInput1*Integer.parseInt(splitStr[i]));
 		}
+		System.out.println(userInput1 * userInput2);		
 	}
 }
